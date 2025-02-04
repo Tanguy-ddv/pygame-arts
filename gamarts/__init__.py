ad02534c1ab43ec1efc043d8eed25a16f01f4b13
Tanguy-ddv <tanguy.dugas01@gmail.com>
2025-02-04 01:16:26 +0100

    chg: import all arts at the root for more simplicity

--- gamarts/__init__.py
+++ gamarts/__init__.py
@@ -1,3 +1,3 @@
-import gamarts.art as art
+from gamarts.art import GIFFile, ImageFile, ImageFolder, Rectangle, RoundedRectangle, Circle, Ellipse, Polygon, TexturedCircle, TexturedEllipse, TexturedPolygon, TexturedRoundedRectangle
 import gamarts.mask as mask
 import gamarts.transform as transform
