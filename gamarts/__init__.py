282489ab5195b03e9f492c4a5f6613a185c16071
Tanguy-ddv <tanguy.dugas01@gmail.com>
2025-02-04 15:37:48 +0100

    new: add Art at the root for easy type hinting

--- gamarts/__init__.py
+++ gamarts/__init__.py
@@ -1,3 +1,3 @@
-from gamarts.art import GIFFile, ImageFile, ImageFolder, Rectangle, RoundedRectangle, Circle, Ellipse, Polygon, TexturedCircle, TexturedEllipse, TexturedPolygon, TexturedRoundedRectangle
+from gamarts.art import GIFFile, ImageFile, ImageFolder, Rectangle, RoundedRectangle, Circle, Ellipse, Polygon, TexturedCircle, TexturedEllipse, TexturedPolygon, TexturedRoundedRectangle, Art
 import gamarts.mask as mask
 import gamarts.transform as transform
