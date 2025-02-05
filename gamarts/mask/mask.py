37e04c3c02084dd3bbfaabb3b2ba26cbd9f96777
Tanguy-ddv <tanguy.dugas01@gmail.com>
2025-02-05 13:12:44 +0100

    chg: remove the need for the width and height to be given at the constructor of the mask

--- gamarts/mask/mask.py
+++ gamarts/mask/mask.py
@@ -1,7 +1,7 @@
 """This mask submodule contains the bases for masks and geometrical masks."""
 
 from abc import ABC, abstractmethod
-from ZOCallable import ZOCallable, verify_ZOCallable
+from ZOCallable import ZOZOCallable, verify_ZOZOCallable
 from ZOCallable.functions import linear
 import numpy as np
 from pygame import Surface, surfarray as sa, SRCALPHA, draw, Rect
@@ -9,31 +9,18 @@ from pygame import Surface, surfarray as sa, SRCALPHA, draw, Rect
 class Mask(ABC):
     """Mask is an abstract class for all masks."""
 
-    def __init__(self, width: int, height: int) -> None:
+    def __init__(self) -> None:
         super().__init__()
         self._loaded = False
-        self._width = width
-        self._height = height
         self.matrix: np.ndarray = None
-        self.settings = None
-
-    @property
-    def width(self):
-        """The width of the mask."""
-        return self._width
-
-    @property
-    def height(self):
-        """The height of the mask."""
-        return self._height
 
     @abstractmethod
-    def _load(self, **ld_kwargs):
+    def _load(self, width: int, height: int, **ld_kwargs):
         raise NotImplementedError()
 
-    def load(self, **ld_kwargs):
+    def load(self, width: int, height: int, **ld_kwargs):
         """Load the mask."""
-        self._load(**ld_kwargs)
+        self._load(width, height,**ld_kwargs)
         self._loaded = True
 
     def unload(self):
@@ -48,10 +35,6 @@ class Mask(ABC):
         """Return True if the mask is loaded, False otherwise."""
         return self._loaded
 
-    def get_size(self) -> tuple[int, int]:
-        """Return the size of the mask"""
-        return (self.width, self.height)
-
     def __bool__(self):
         return True
 
@@ -60,113 +43,110 @@ class Mask(ABC):
         Return the value of the matrix at this coordinate.
         """
         if not self.is_loaded():
-            self.load(self.settings)
+            return False
         return not bool(self.matrix[int(pos[0]), int(pos[1])])
 
     def set_at(self, pos: tuple[int, int], value: float):
         """
         Set a new value for the matrix at this coordinate.
         """
-        if not self.is_loaded():
-            self.load(self.settings)
-        self.matrix[pos] = min(1, max(0, value))
+        if self.is_loaded():
+            self.matrix[pos] = min(1, max(0, value))
 
     def not_null_columns(self):
         """Return the list of indices of the columns that have at least one value different from 0."""
-        if not self.is_loaded():
-            self.load(self.settings)
-        return np.where(self.matrix.any(axis=0))[0]
+        if self.is_loaded():
+            return np.where(self.matrix.any(axis=0))[0]
+        return []
 
     def not_null_rows(self):
         """Return the list of indices of the rows that have at least one value different from 0."""
-        if not self.is_loaded():
-            self.load(self.settings)
-        return np.where(self.matrix.any(axis=1))[0]
+        if self.is_loaded():
+            return np.where(self.matrix.any(axis=1))[0]
+        return []
 
     def is_empty(self):
         """Return True if all the pixels in the mask are set to 0."""
         if not self.is_loaded():
-            self.load(self.settings)
+            return True
         return np.sum(self.matrix) == 0
 
     def is_full(self):
         """Return True if all the pixels in the mask are set to 1."""
         if not self.is_loaded():
-            self.load(self.settings)
-        return np.sum(self.matrix) == self.height*self.width
+            return False
+        return np.sum(self.matrix) == self.matrix.size
 
 class MatrixMask(Mask):
-    """A matrix mask is a mask based on a matrix."""
+    """A matrix mask is a mask based on a matri, this matrix can be changed."""
 
-    def __init__(self, width: int, height: int, matrix: np.ndarray) -> None:
-        super().__init__(width, height)
-        self.matrix = np.clip(matrix, 0, 1)
+    def __init__(self, matrix: np.ndarray) -> None:
+        super().__init__()
+        self._matrix = np.clip(matrix, 0, 1)
 
-    def unload(self):
-        """Don't do anything as we want to keep the matrix."""
-
-    def _load(self, **ld_kwargs):
-        """Don't do anything as the matrix is already loaded."""
-
-class FreeMask(Mask):
-    """A FreeMask is a mask whose matrix can be freely and explicitly change during the game."""
-
-    def __init__(self, width, height, initial_matrix):
-        super().__init__(width, height)
-        self.__initial_matrix = initial_matrix
-
-    def _load(self, settings):
-        self.matrix = self.__initial_matrix
-
+    def _load(self, width: int, height: int, **ld_kwargs):
+        """Pad and crop the matrix to match the size."""
+        self.matrix = np.pad(
+            self._matrix, [
+                (0, max(0, width - self._matrix.shape[0])),
+                (0, max(0, height - self._matrix.shape[1]))
+            ],
+            'edge'
+        )[0: width, 0:height]
+    
     def update_matrix(self, new_matrix: np.ndarray):
         """Update the current matrix with a user-defined matrix. Both matrices should have the same shape."""
-        if self.matrix.shape == new_matrix.shape:
-            self.matrix = new_matrix
-        else:
-            raise ValueError(f"The matrices have two different shapes: {self.matrix.shape}, {new_matrix.shape}")
+        self._matrix = new_matrix
+        self._load(*self.matrix.shape)
 
 class Circle(Mask):
     """A Circle is a mask with two values: 0 in the circle and 1 outside."""
 
-    def __init__(self, width: int, height: int, radius: float, center: tuple[int, int] = None):
-        super().__init__(width, height)
+    def __init__(self, radius: float | int, center: tuple[float, float] | tuple[int, int] = (0.5, 0.5)):
+        super().__init__()
         self.radius = radius
-        if center is None:
-            center = width/2 - 0.5, height/2 - 0.5
         self.center = center
 
-    def _load(self, **ld_kwargs):
-        grid_x, grid_y = np.ogrid[:self._width, :self._height]
-        distances = np.sqrt((grid_x - self.center[0]) ** 2 + (grid_y - self.center[1]) ** 2)
-        self.matrix = (distances > self.radius).astype(int)
+    def _load(self, width: int, height: int, **ld_kwargs):
+        grid_x, grid_y = np.ogrid[:width, :height]
+        center = (
+            self.center[0]*width if 0 <= self.center[0] <= 1 else self.center[0],
+            self.center[1]*height if 0 <= self.center[1] <= 1 else self.center[1]
+        )
+        distances = np.sqrt((grid_x - center[0] - 0.5) ** 2 + (grid_y - center[1] - 0.5) ** 2)
+        radius = self.radius*min(height, width) if  0 <= self.radius <= 1 else self.radius
+        self.matrix = (distances > radius).astype(int)
 
 class Ellipse(Mask):
     """An Ellipsis is a mask with two values: 0 in the ellipsis and 1 outside."""
 
-    def __init__(self, width: int, height: int, x_radius: int, y_radius: int, center: tuple[int, int] = None):
-        super().__init__(width, height)
-        self.x_radius = x_radius
-        self.y_radius = y_radius
-        if center is None:
-            center = width/2 - 0.5, height/2 - 0.5
+    def __init__(self, radius_x: int, radius_y: int, center: tuple[float, float] = (0.5, 0.5)):
+        super().__init__()
+        self.radius_x = radius_x
+        self.radius_y = radius_y
         self.center = center
 
-    def _load(self, **ld_kwargs):
-        grid_y, grid_x = np.ogrid[:self._height, :self._width]
-        distances = np.sqrt((grid_x - self.center[0]) ** 2 / self.x_radius**2 + (grid_y - self.center[1]) ** 2 / self.y_radius**2)
+    def _load(self, width: int, height: int, **ld_kwargs):
+        grid_y, grid_x = np.ogrid[:height, :width]
+        center = (
+            self.center[0]*width if 0 <= self.center[0] <= 1 else self.center[0],
+            self.center[1]*height if 0 <= self.center[1] <= 1 else self.center[1]
+        )
+        radius_x = self.radius_x*width if  0 <= self.radius_x <= 1 else self.radius_x
+        radius_y = self.radius_y*height if  0 <= self.radius_y <= 1 else self.radius_y
+        distances = np.sqrt((grid_x - center[0] - 0.5) ** 2 / radius_x**2 + (grid_y - center[1] - 0.5) ** 2 / radius_y**2)
+
         self.matrix = (distances > 1).astype(int)
 
 class Rectangle(Mask):
     """A Rectangle is a mask with two values: 0 inside the rectangle and 1 outside."""
 
-    def __init__(self, width: int, height: int, left: int, top: int, right: int, bottom: int):
+    def __init__(self, left: int | float, top: int | float, right: int | float, bottom: int | float):
         """
         A Rectangle is a mask with two values: 0 inside the rectangle and 1 outside.
         
         Params:
         ----
-        - width: int, the width of the mask.
-        - height: int, the height of the mask.
         - left: int, the coordinate of the left of the rectangle, included.
         - top: int, the coordinate of the top of the rectangle, included.
         - right: int, the coordinate of the right of the rectangle, included.
@@ -183,15 +163,19 @@ class Rectangle(Mask):
              [1 1 1 1 1 1]]
         """
 
-        super().__init__(width, height)
+        super().__init__()
         self.left = left
         self.top = top
         self.right = right
         self.bottom = bottom
 
-    def _load(self, **ld_kwargs):
-        grid_y, grid_x = np.ogrid[:self._height, :self._width]
-        self.matrix = 1 - ((self.left <= grid_x) & (grid_x <= self.right) & (self.top <= grid_y) & (grid_y <= self.bottom)).astype(int)
+    def _load(self, width:int, height:int, **ld_kwargs):
+        grid_y, grid_x = np.ogrid[:height, :width]
+        left = self.left*width if 0 <= self.left <= 1 and isinstance(self.left, float) else self.left
+        right = self.right*width if 0 <= self.right <= 1 and isinstance(self.right, float) else self.right
+        top = self.top*height if 0 <= self.top <= 1 and isinstance(self.top, float) else self.top
+        bottom = self.bottom*height if 0 <= self.bottom <= 1 and isinstance(self.bottom, float) else self.bottom
+        self.matrix = 1 - ((left <= grid_x) & (grid_x <= right) & (top <= grid_y) & (grid_y <= bottom)).astype(int)
 
 class Polygon(Mask):
     """
@@ -200,30 +184,34 @@ class Polygon(Mask):
     the polygon is cropped.
     """
 
-    def __init__(self, width: int, height: int, points: list[tuple[int, int]]) -> None:
-        super().__init__(width, height)
+    def __init__(self, points: list[tuple[int, int]]) -> None:
+        super().__init__()
 
         self.points = points
 
-    def _load(self, **ld_kwargs):
-        surf = Surface((self._width, self._height), SRCALPHA)
+    def _load(self, width: int, height: int, **ld_kwargs):
+        surf = Surface((width, height), SRCALPHA)
         draw.polygon(surf, (0, 0, 0, 255), self.points)
         self.matrix = 1 - sa.array_alpha(surf)/255
 
 class RoundedRectangle(Mask):
     """A RoundedRectangle mask is a mask with two values: 0 inside of the rectangle with rounded vertexes, and 1 outside."""
 
-    def __init__(self, width: int, height: int, left: int, top: int, right: int, bottom: int, radius: int):
-        super().__init__(width, height)
+    def __init__(self, left: int | float, top: int | float, right: int | float, bottom: int | float, radius: int):
+        super().__init__()
         self.left = left
         self.top = top
         self.right = right
         self.bottom = bottom
         self.radius = radius
 
-    def _load(self, **ld_kwargs):
-        surf = Surface((self._width, self._height), SRCALPHA)
-        draw.rect(surf, (0, 0, 0, 255), Rect(self.left, self.top, self.right - self.left, self.bottom - self.top), 0, self.radius)
+    def _load(self, width: int, height: int, **ld_kwargs):
+        surf = Surface((width, height), SRCALPHA)
+        left = self.left*width if 0 <= self.left <= 1 and isinstance(self.left, float) else self.left
+        right = self.right*width if 0 <= self.right <= 1 and isinstance(self.right, float) else self.right
+        top = self.top*height if 0 <= self.top <= 1 and isinstance(self.top, float) else self.top
+        bottom = self.bottom*height if 0 <= self.bottom <= 1 and isinstance(self.bottom, float) else self.bottom
+        draw.rect(surf, (0, 0, 0, 255), Rect(left, top, right - left + 1, bottom - top + 1), 0, self.radius)
         self.matrix = 1 - sa.array_alpha(surf)/255
 
 class GradientCircle(Mask):
@@ -236,27 +224,30 @@ class GradientCircle(Mask):
 
     def __init__(
             self,
-            width: int,
-            height: int,
-            inner_radius: int,
-            outer_radius: int,
-            transition: ZOCallable = linear,
-            center: tuple[int, int] = None
+            inner_radius: int | float,
+            outer_radius: int | float,
+            transition: ZOZOCallable = linear,
+             center: tuple[float, float] | tuple[int, int] = (0.5, 0.5)
         ):
-        super().__init__(width, height)
+        super().__init__()
         self.inner_radius = inner_radius
         self.outer_radius = outer_radius
-        verify_ZOCallable(transition)
+        verify_ZOZOCallable(transition, test_vectorizaiton=True)
         self.transition = transition
-
-        if center is None:
-            center = width/2 - 0.5, height/2 - 0.5
         self.center = center
 
-    def _load(self, **ld_kwargs):
-        grid_x, grid_y = np.ogrid[:self._width, :self._height]
-        distances = np.sqrt((grid_x - self.center[0]) ** 2 + (grid_y - self.center[1]) ** 2)
-        self.matrix = np.clip((distances - self.inner_radius)/(self.outer_radius - self.inner_radius), 0, 1)
+    def _load(self, width:int, height: int, **ld_kwargs):
+
+        center = (
+            self.center[0]*width if 0 <= self.center[0] <= 1 else self.center[0],
+            self.center[1]*height if 0 <= self.center[1] <= 1 else self.center[1]
+        )
+        distances = np.sqrt((grid_x - center[0] - 0.5) ** 2 + (grid_y - center[1] - 0.5) ** 2)
+        inner_radius = self.inner_radius*min(height, width) if  0 <= self.inner_radius <= 1 else self.inner_radius
+        outer_radius = self.outer_radius*min(height, width) if  0 <= self.outer_radius <= 1 else self.outer_radius
+        grid_x, grid_y = np.ogrid[:width, :height]
+        distances = np.sqrt((grid_x - center[0] - 0.5) ** 2 + (grid_y - center[1] - 0.5) ** 2)
+        self.matrix = np.clip((distances - inner_radius)/(outer_radius - inner_radius), 0, 1)
         self.matrix = self.transition(self.matrix)
 
 class GradientRectangle(Mask):
@@ -269,30 +260,18 @@ class GradientRectangle(Mask):
 
     def __init__(
         self,
-        width: int,
-        height: int,
         inner_left: int,
         inner_right: int,
         inner_top: int,
         inner_bottom: int,
-        outer_left: int = None,
-        outer_right: int = None,
-        outer_top: int = None,
-        outer_bottom: int = None,
-        transition: ZOCallable = linear
+        outer_left: int = 0.,
+        outer_right: int = 1.,
+        outer_top: int = 0.,
+        outer_bottom: int = 1.,
+        transition: ZOZOCallable = linear
     ):
 
-        super().__init__(width, height)
-
-        if outer_left is None:
-            outer_left = 0
-        if outer_right is None:
-            outer_right = width - 1
-        if outer_top is None:
-            outer_top = 0
-        if outer_bottom is None:
-            outer_bottom = height - 1
-
+        super().__init__()
 
         if outer_bottom < inner_bottom or outer_top > inner_top or outer_left > inner_left or outer_right < inner_right:
             raise ValueError(
@@ -311,15 +290,25 @@ class GradientRectangle(Mask):
         self.outer_bottom = outer_bottom
         self.outer_top = outer_top
 
-        verify_ZOCallable(transition)
+        verify_ZOZOCallable(transition, test_vectorizaiton=True)
         self.transition = transition
 
-    def _load(self, **ld_kwargs):
-        y_indices, x_indices = np.meshgrid(np.arange(self.height), np.arange(self.width), indexing='ij')
+    def _load(self, width: int, height: int, **ld_kwargs):
+        y_indices, x_indices = np.meshgrid(np.arange(height), np.arange(width), indexing='ij')
 
-        left_dist = np.clip((self.inner_left - x_indices) / (self.inner_left - self.outer_left + 1), 0, 1)
-        right_dist = np.clip((x_indices - self.inner_right) / (self.outer_right - self.inner_right + 1), 0, 1)
-        top_dist = np.clip((self.inner_top - y_indices) / (self.inner_top - self.outer_top + 1), 0, 1)
-        bottom_dist = np.clip((y_indices - self.inner_bottom) / (self.outer_bottom - self.inner_bottom + 1), 0, 1)
+        inner_left = self.inner_left*width if 0 <= self.inner_left <= 1 and isinstance(self.inner_left, float) else self.inner_left
+        inner_right = self.inner_right*width if 0 <= self.inner_right <= 1 and isinstance(self.inner_right, float) else self.inner_right
+        inner_top = self.inner_top*height if 0 <= self.inner_top <= 1 and isinstance(self.inner_top, float) else self.inner_top
+        inner_bottom = self.inner_bottom*height if 0 <= self.inner_bottom <= 1 and isinstance(self.inner_bottom, float) else self.inner_bottom
+
+        outer_left = self.outer_left*width if 0 <= self.outer_left <= 1 and isinstance(self.outer_left, float) else self.outer_left
+        outer_right = self.outer_right*width if 0 <= self.outer_right <= 1 and isinstance(self.outer_right, float) else self.outer_right
+        outer_top = self.outer_top*height if 0 <= self.outer_top <= 1 and isinstance(self.outer_top, float) else self.outer_top
+        outer_bottom = self.outer_bottom*height if 0 <= self.outer_bottom <= 1 and isinstance(self.outer_bottom, float) else self.outer_bottom
+
+        left_dist = np.clip((inner_left - x_indices) / (inner_left - self.outer_left + 1), 0, 1)
+        right_dist = np.clip((x_indices - inner_right) / (outer_right - self.inner_right + 1), 0, 1)
+        top_dist = np.clip((inner_top - y_indices) / (inner_top - outer_top + 1), 0, 1)
+        bottom_dist = np.clip((y_indices - inner_bottom) / (outer_bottom - inner_bottom + 1), 0, 1)
 
         self.matrix = self.transition(np.clip(np.sqrt(left_dist**2 + right_dist**2 + top_dist**2 + bottom_dist**2), 0, 1))
