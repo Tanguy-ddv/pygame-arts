efb60059b1d818bc5add205aed01a90c1fb899c6
Tanguy-ddv <tanguy.dugas01@gmail.com>
2025-02-18 10:16:28 +0100

    new: add docstrings to the drawing transfo

--- gamarts/transform/drawing.py
+++ gamarts/transform/drawing.py
@@ -1,19 +1,31 @@
 from typing import Sequence
-from pygame import Surface, Rect, Color
+from pygame import Surface, Rect
 from pygamecv import rectangle, line, lines, polygon, circle, ellipse, pie, arc, rounded_rectangle
 from .transformation import Transformation
+from .._common import ColorValue
 
 class DrawCircle(Transformation):
     """Draw a circle on the art."""
 
     def __init__(
         self,
-        color: Color,
+        color: ColorValue,
         radius: int,
         center: tuple[int, int],
         thickness: int = 0,
         allow_antialias: bool = True
     ) -> None:
+        """
+        Draw a circle on the art.
+
+        Params:
+        ----
+        - color: ColorValue, the color used to draw the circle. It can have an alpha channel != 255.
+        - radius: int, the radius of the circle
+        - center: tuple[int, int], the center of the circle
+        - thickness: int, the thickness of the line used to draw the circle. If 0, the circle is filled.
+        - allow_antialias: bool = True, if False, even with 'antialias' : True as a loading kwarg, the drawing will be done without antialias.
+        """
         super().__init__()
 
         self.radius = radius
@@ -38,11 +50,22 @@ class DrawRectangle(Transformation):
     """Draw a rectangle on the art."""
     def __init__(
         self,
-        color: Color,
+        color: ColorValue,
         rect: Rect,
         thickness: int,
         allow_antialias: bool = True
     ) -> None:
+        """
+        Draw a rectangle on the art.
+
+        Params:
+        ---
+        - color: ColorValue, the color used to draw the rectangle. It can have an alpha channel != 255.
+        - rect: pygame.Rect, the rectangle representing the edges of the rectangle.
+        - thickness: int, the thickness of the line used to draw the rectangle. If 0, the rectangle is filled.
+        - allow_antialias: bool = True, if False, even with 'antialias' : True as a loading kwarg, the drawing will be done without antialias.
+        """
+
         super().__init__()  
         self.color = color
         self.rect = Rect(rect)
@@ -65,7 +88,7 @@ class DrawRoundedRectantle(Transformation):
     """Draw a rectangle on the art, with rounded corners."""
     def __init__(
         self,
-        color: Color,
+        color: ColorValue,
         rect: Rect,
         top_left: int,
         top_right: int = None,
@@ -74,6 +97,18 @@ class DrawRoundedRectantle(Transformation):
         thickness: int = 0,
         allow_antialias: bool = True
     ) -> None:
+        """
+        Draw a rectangle on the art, with rounded corners.
+
+        Params:
+        ---
+        - color: ColorValue, the color used to draw the rounded rectangle. It can have an alpha channel != 255.
+        - rect: pygame.Rect, the rectangle representing the edges of the rectangle.
+        - top_left, top_right, bottom_left, bottom_right: the radii of the rounded corners.
+        If one of the 3 last is None, the value for the top_left corner is used instead.
+        - thickness: int, the thickness of the line used to draw the rounded rectangle. If 0, the rounded rectangle is filled.
+        - allow_antialias: bool = True, if False, even with 'antialias' : True as a loading kwarg, the drawing will be done without antialias.
+        """
         super().__init__()  
         self.color = color
         self.rect = Rect(rect)
@@ -107,7 +142,7 @@ class DrawEllipse(Transformation):
 
     def __init__(
         self,
-        color: Color,
+        color: ColorValue,
         radius_x: int,
         radius_y: int,
         center: tuple[int, int],
@@ -115,6 +150,19 @@ class DrawEllipse(Transformation):
         angle: int=0,
         allow_antialias: bool = True
     ):
+        """
+        Draw an ellipse on the art.
+
+        Params:
+        ----
+        - color: ColorValue, the color used to draw the ellipse. It can have an alpha channel != 255.
+        - radius_x: int, the horizontal radius of the ellipse, before rotation.
+        - radius_y: int, the vertical radius of the ellipse, before rotation.
+        - center: tuple[int, int], the center of the ellipse
+        - thickness: int, the thickness of the line used to draw the ellipse. If 0, the ellipse is filled.
+        - angle: int = 0, the angle in degrees by which the ellipse should be rotated, counterclockwise.
+        - allow_antialias: bool = True, if False, even with 'antialias' : True as a loading kwarg, the drawing will be done without antialias.
+        """
         self.color = color
         self.radius_x = radius_x
         self.radius_y = radius_y
@@ -140,11 +188,21 @@ class DrawPolygon(Transformation):
 
     def __init__(
         self,
-        color: Color,
+        color: ColorValue,
         points: Sequence[tuple[int, int]],
         thickness: int = 0,
         allow_antialias: bool = True
     ) -> None:
+        """
+        Draw a polygon on the art.
+
+        Params:
+        ---
+        - color: ColorValue, the color used to draw the polygon. It can have an alpha channel != 255.
+        - points: Sequence[tuple[int, int]], the sequence of points.
+        - thickness: int, the thickness of the line used to draw the polygon. If 0, the polygon is filled.
+        - allow_antialias: bool = True, if False, even with 'antialias' : True as a loading kwarg, the drawing will be done without antialias.
+        """
         super().__init__()
 
         self.color = color
@@ -170,7 +228,19 @@ class DrawPolygon(Transformation):
 class DrawLine(Transformation):
     """Draw one line on the art."""
 
-    def __init__(self, color: Color, p1: tuple[int, int], p2: tuple[int, int], thickness: int = 1, allow_antialias: bool = True) -> None:
+    def __init__(self, color: ColorValue, p1: tuple[int, int], p2: tuple[int, int], thickness: int = 1, allow_antialias: bool = True) -> None:
+        """
+        Draw one line on the art.
+        
+        Params:
+        ---
+        - color: ColorValue, the color used to draw the line. It can have an alpha channel != 255.
+        - p1: tuple[int, int], the position of start of the line.
+        - p2: tuple[int, int], the position of the end of the line.
+        - thickness: int, the thickness of the line used to draw the line.
+        - allow_antialias: bool = True, if False, even with 'antialias' : True as a loading kwarg, the drawing will be done without antialias.
+        """
+
         self.color = color
         self.p1 = p1
         self.p2 = p2
@@ -196,7 +266,18 @@ class DrawLine(Transformation):
 class DrawLines(Transformation):
     """Draw lines on the art."""
 
-    def __init__(self, color: Color, points: Sequence[tuple[int, int]], thickness: int = 1, closed: bool = False, allow_antialias: bool = True) -> None:
+    def __init__(self, color: ColorValue, points: Sequence[tuple[int, int]], thickness: int = 1, closed: bool = False, allow_antialias: bool = True) -> None:
+        """
+        Draw lines on the art. This is faster than drawing the lines one by one.
+
+        Params:
+        ---
+        - color: ColorValue, the color used to draw the lines. It can have an alpha channel != 255.
+        - points: Sequence[tuple[int, int]], the sequence of points.
+        - thickness: int, the thickness of the line used to draw the lines.
+        - closed: bool, If True, the last points is linked to the first point with a line.
+        - allow_antialias: bool = True, if False, even with 'antialias' : True as a loading kwarg, the drawing will be done without antialias.
+        """
         self.color = color
         self.points = points
         self.thickness = thickness
@@ -224,16 +305,31 @@ class DrawArc(Transformation):
 
     def __init__(
         self,
-        color: Color,
-        ellipsis_center: tuple[int, int],
+        color: ColorValue,
         radius_x: int,
         radius_y: int,
+        ellipsis_center: tuple[int, int],
         from_angle: int,
         to_angle: int,
-        angle: int = 0,
         thickness: int = 1,
+        angle: int = 0,
         allow_antialias: bool = True
     ) -> None:
+        """
+        Draw an arc on the art. An arc is a part of an ellipse.
+
+        Params:
+        ----
+        - color: ColorValue, the color used to draw the arc. It can have an alpha channel != 255.
+        - radius_x: int, the horizontal radius of the ellipse, before rotation.
+        - radius_y: int, the vertical radius of the ellipse, before rotation.
+        - ellipsis_center: tuple[int, int], the center of the ellipse.
+        - from_angle: the starting angle of the arc, in degrees
+        - to_angle: the ending angle of the arc, in degrees. The arc is drawn counterclockwise.
+        - thickness: int, the thickness of the line used to draw the arc. If 0, a filled pie is drawn.
+        - angle: int = 0, the angle in degrees by which the ellipse should be rotated, counterclockwise.
+        - allow_antialias: bool = True, if False, even with 'antialias' : True as a loading kwarg, the drawing will be done without antialias.
+        """
         self.color = color
         self.thickness = thickness
         self.ellipsis_center = ellipsis_center
@@ -243,7 +339,6 @@ class DrawArc(Transformation):
         self.to_angle = to_angle
         self.angle = angle
         self.allow_antialias = allow_antialias
-
         super().__init__()
 
     def apply(self, surfaces: tuple[Surface], durations: tuple[int], introduction: int, index: int, width: int, height: int, **ld_kwargs):
@@ -257,20 +352,36 @@ class DrawArc(Transformation):
         return self.rx*self.ry*4*length*4 # Once for the color draw, twice for the apha rendering (copy and addWeighted) and once for the final blit.
 
 class DrawPie(Transformation):
-    """Draw an arc on the art."""
+    """Draw a pie on the art."""
 
     def __init__(
         self,
-        color: Color,
-        ellipsis_center: tuple[int, int],
+        color: ColorValue,
         radius_x: int,
         radius_y: int,
+        ellipsis_center: tuple[int, int],
         from_angle: int,
         to_angle: int,
-        angle: int = 0,
         thickness: int = 1,
+        angle: int = 0,
         allow_antialias: bool = True
     ) -> None:
+        """
+        Draw a pie on the art. A pie is an arc where the start and end points are linked to the center of the ellipse.
+
+        Params:
+        ----
+        - color: ColorValue, the color used to draw the pie. It can have an alpha channel != 255.
+        - radius_x: int, the horizontal radius of the ellipse, before rotation.
+        - radius_y: int, the vertical radius of the ellipse, before rotation.
+        - ellipsis_center: tuple[int, int], the center of the ellipse.
+        - from_angle: the starting angle of the arc, in degrees
+        - to_angle: the ending angle of the arc, in degrees. The arc is drawn counterclockwise.
+        - thickness: int, the thickness of the line used to draw the pie. If 0, the pie is filled.
+        - angle: int = 0, the angle in degrees by which the ellipse should be rotated, counterclockwise.
+        - allow_antialias: bool = True, if False, even with 'antialias' : True as a loading kwarg, the drawing will be done without antialias.
+        """
+
         self.color = color
         self.thickness = thickness
         self.ellipsis_center = ellipsis_center
@@ -280,7 +391,6 @@ class DrawPie(Transformation):
         self.to_angle = to_angle
         self.angle = angle
         self.allow_antialias = allow_antialias
-
         super().__init__()
 
     def apply(self, surfaces: tuple[Surface], durations: tuple[int], introduction: int, index: int, width: int, height: int, **ld_kwargs):
